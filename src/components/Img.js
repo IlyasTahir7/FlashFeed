1343446879bbb8c374d3c50db0143ea1830b5a2d
Ilyas <143258499+IlyasTahir7@users.noreply.github.com>
2023-09-06 19:10:40 +0500

    first commit

---
+++ src/components/Img.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import background from "./background.jpg"
+export default function Img() {
+  return (
+    <div>
+
+        <img src={background} class="img-fluid" alt="background image"></img>
+        </div>
+  )
+}
---
+++ src/components/TestHome.js
@@ -0,0 +1,8 @@
+// TestHome.js
+import React from 'react';
+
+const TestHome = () => {
+  return <div>This is a test home page.</div>;
+};
+
+export default TestHome;
